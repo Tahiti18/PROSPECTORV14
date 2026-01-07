3950c37b349c2ea1f2b77fdcf022c3f5349b9a45
Tahiti18 <35585881+Tahiti18@users.noreply.github.com>
2026-01-07 19:20:17 +0200

    feat: Allow all hosts for dev server and preview
    Enables access to the development server and preview from any IP address, improving local development flexibility.

--- vite.config.ts
+++ vite.config.ts
@@ -114,10 +114,12 @@ export default defineConfig(({ mode }) => {
     server: {
       host: '0.0.0.0',
       port: Number(process.env.PORT) || 5173,
+      allowedHosts: true,
     },
     preview: {
         host: '0.0.0.0',
         port: Number(process.env.PORT) || 4173,
+        allowedHosts: true,
     }
   };
 });
